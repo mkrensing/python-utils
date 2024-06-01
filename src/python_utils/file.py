1d97e7580ec6714272fedf9f18fe39433efff814
mkrensing <martin@krensing.de>
2024-06-01 10:34:48 +0200

    APPLICATION_PATH lookup

--- src/python_utils/file.py
+++ src/python_utils/file.py
@@ -3,14 +3,29 @@ from pathlib import Path
 from python_utils.env import inject_environment
 import sys
 
+FOUND_APPLICATION_PATH: str = ""
 
 def lookup_application_path() -> str:
+    global FOUND_APPLICATION_PATH
+
+    if FOUND_APPLICATION_PATH:
+        return FOUND_APPLICATION_PATH
+
     # determine if application is a script file or frozen exe
     if getattr(sys, 'frozen', False):
-        return os.path.dirname(sys.executable)
+        FOUND_APPLICATION_PATH = os.path.dirname(sys.executable)
+    else:
+        this_script_directory = os.path.dirname(os.path.realpath(__file__))
+        FOUND_APPLICATION_PATH = os.path.realpath(path_until_last_match(this_script_directory, "venv"))
 
-    this_script_directory = os.path.dirname(os.path.realpath(__file__))
-    return f"{this_script_directory}/../../../.."
+    return FOUND_APPLICATION_PATH
+
+
+def path_until_last_match(path: str, match: str) -> str:
+    position = path.rfind(match)
+    if position == -1:
+        raise Exception(f"'{match}' not found in path: '{path}'")
+    return path[:position]
 
 
 @inject_environment({"APPLICATION_PATH": lookup_application_path()})
