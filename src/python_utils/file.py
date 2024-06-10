31567fabe2fa8ebe1b0ca00311be4ee51fea2a3a
mkrensing <martin@krensing.de>
2024-06-10 13:01:40 +0200

    adjust application path

--- src/python_utils/file.py
+++ src/python_utils/file.py
@@ -18,6 +18,8 @@ def lookup_application_path() -> str:
     else:
         this_script_directory = os.path.dirname(os.path.realpath(__file__))
         FOUND_APPLICATION_PATH = os.path.realpath(path_until_last_match(this_script_directory, "venv"))
+        if not FOUND_APPLICATION_PATH:
+            FOUND_APPLICATION_PATH = os.path.dirname(sys.argv[0])
 
     return FOUND_APPLICATION_PATH
 
@@ -25,7 +27,8 @@ def lookup_application_path() -> str:
 def path_until_last_match(path: str, match: str) -> str:
     position = path.rfind(match)
     if position == -1:
-        raise Exception(f"'{match}' not found in path: '{path}'")
+        return ""
+
     return path[:position]
 
 
