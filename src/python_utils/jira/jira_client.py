ff8d82b6d5c4a6cc527a238e88bdeaa938408ed8
mkrensing <martin@krensing.de>
2024-09-06 10:06:56 +0200

    jira_client search

--- src/python_utils/jira/jira_client.py
+++ src/python_utils/jira/jira_client.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 from typing import List, Dict, Callable
 
@@ -39,6 +40,45 @@ class QueryCache:
                 self.db.close()
 
 
+class Pagination:
+
+    def __init__(self, jira: JIRA, jql: str, expand: str, max_page_size: int, issues: List[Dict]=None):
+        self.jira = jira
+        self.jql = jql
+        self.expand = expand
+        self.max_page_size = max_page_size
+        self.has_next_page = True
+        self.next_page_start_at = 0
+        self.issues = issues
+
+    def has_next(self) -> bool:
+        return self.has_next_page
+
+    def get_next(self) -> List[Dict]:
+
+        if self.issues:
+            self.has_next_page = False
+            return self.issues
+
+        if not self.has_next_page:
+            return []
+
+        result_set = self.jira.search_issues(self.jql, expand=self.expand, maxResults=self.max_page_size, startAt=self.next_page_start_at)
+        self.has_next_page = result_set.startAt < result_set.total
+        self.next_page_start_at = result_set.startAt + len(result_set)
+
+        return Pagination.print_size_of([ issue.raw for issue in result_set ])
+
+    @staticmethod
+    def print_size_of(issues: List[Dict]) -> List[Dict]:
+        print(f"Jira returned {len(issues)} issues with a size of {Pagination.get_size_in_mb(issues)}")
+        return issues
+
+    @staticmethod
+    def get_size_in_mb(object) -> str:
+        return f"{round(sys.getsizeof(object) / 1000000)} MB"
+
+
 class JiraClient:
 
     def __init__(self, hostname: str, query_cache_filename: str, test_mode: bool, max_result_size=700):
@@ -49,6 +89,15 @@ class JiraClient:
 
     def set_test_mode(self, test_mode: bool):
         self.test_mode = test_mode
+
+    def search(self, jql: str, access_token: str, page_size=200, use_cache=False, expand="changelog") -> Pagination:
+        jira = JIRA(server=self.hostname, token_auth=access_token)
+        issues = None
+        if self.test_mode or use_cache:
+            issues = self.query_cache.get_issues(jql)
+
+        return Pagination(jira=jira, jql=jql, expand=expand, max_page_size=page_size, issues=issues)
+
 
     def get_issues(self, jql: str, access_token: str, use_cache: False, page_size=200) -> List[Dict]:
 
@@ -85,12 +134,17 @@ class JiraClient:
             raise Exception(f"Resultset is to large. Size: {result_set.total} over {self.max_result_size}")
 
         while result_set.startAt < result_set.total:
-            for issue in result_set:
-                issues.append(issue.raw)
+            print(f"Jira returned {len(result_set)} issues with a size of {self.get_size_in_mb(result_set)}")
+            issues.extend([ issue.raw for issue in result_set ])
             next_page_start_at = result_set.startAt + len(result_set)
             result_set = jira.search_issues(jql, expand=expand, maxResults=page_size, startAt=next_page_start_at)
 
         return issues
+
+    @staticmethod
+    def get_size_in_mb(object) -> str:
+        return f"{round(sys.getsizeof(object) / 1000000)} MB"
+
 
     def close(self):
         if self.query_cache:
