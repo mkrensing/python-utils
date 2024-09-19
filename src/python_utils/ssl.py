9620ec009aaa74faf9f2dd6d92715893596ca89e
mkrensing <martin@krensing.de>
2024-09-19 15:50:24 +0200

    ignore_not_found

--- src/python_utils/ssl.py
+++ src/python_utils/ssl.py
@@ -2,9 +2,11 @@ import os
 import certifi
 from python_utils.file import file_exists
 
-def register_root_certificate(root_ca_filename: str):
+def register_root_certificate(root_ca_filename: str, ignore_not_found=False):
 
     if not file_exists(root_ca_filename):
+        if ignore_not_found:
+            return
         raise Exception(f"FileNotFound: {root_ca_filename}")
 
     with open(root_ca_filename, 'rb') as infile:
