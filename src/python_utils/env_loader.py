70e3f4b3e6bd6c11bcd904ab64c30cc447de558b
mkrensing <martin@krensing.de>
2024-09-30 10:12:14 +0200

    load env from file

--- src/python_utils/env_loader.py
+++ src/python_utils/env_loader.py
@@ -2,6 +2,14 @@ import os
 import yaml
 from python_utils.file import lookup_file
 
+def replace_placeholders(value, env_vars):
+
+    while any(f"{{{key}}}" in value for key in env_vars):
+        for key, val in env_vars.items():
+            value = value.replace(f"{{{key}}}", val)
+    return value
+
+
 def load_from_file(relative_filename: str, ignore_not_found=False):
 
     filename = lookup_file(relative_filename)
@@ -14,5 +22,12 @@ def load_from_file(relative_filename: str, ignore_not_found=False):
     with open(filename, 'r') as file:
         config = yaml.safe_load(file)
 
+    env_vars = {}
     for key, value in config.items():
-        os.environ[key] = str(value)
+        env_vars[key] = str(value)
+
+    for key, value in env_vars.items():
+        env_vars[key] = replace_placeholders(value, env_vars)
+
+    for key, value in env_vars.items():
+        os.environ[key] = value
