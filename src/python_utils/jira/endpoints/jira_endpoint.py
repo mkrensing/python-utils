df4a8fb802964336f998f739ecaeebde95c08e3b
mkrensing <martin@krensing.de>
2024-10-08 15:52:56 +0200

    added search endpoint

--- src/python_utils/jira/endpoints/jira_endpoint.py
+++ src/python_utils/jira/endpoints/jira_endpoint.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 from flask import Blueprint, request
 from python_utils.jira.jira_client import JiraClient
 from python_utils.flask.endpoint import response_json, destroy_endpoint, init_endpoint, response_cookie
@@ -6,8 +7,30 @@ from python_utils.env import inject_environment
 from python_utils.file import lookup_file, file_exists
 from python_utils.jira.jira_security import token_required, get_access_token
 from python_utils.jira.jira_security import read_tokens, write_tokens, register_token, logout, is_logged_in
+from typing import Dict
+
 
 jira_endpoint = Blueprint('jira_endpoint', __name__, url_prefix='/rest/jira')
+
+class JiraSearchConfig:
+
+    def __init__(self, jira_config: Dict):
+        self.jira_config = jira_config
+
+    def is_valid(self) -> bool:
+        return "jql" in self.jira_config and \
+            "useCache" in self.jira_config and \
+            "pageSize" in self.jira_config
+
+    def get_jql(self):
+        return self.jira_config["jql"]
+
+    def is_use_cache(self) -> bool:
+        return bool(self.jira_config["useCache"])
+
+    def get_page_size(self) -> int:
+        return self.jira_config["pageSize"]
+
 
 
 @inject_environment(
@@ -33,6 +56,22 @@ def get_sprints_for_project(project_id: str, name_filter: str, activated_date: s
     return response_json(
         jira_client.get_sprints_for_project(project_id, name_filter, activated_date, access_token=get_access_token(), force_reload=force_reload))
 
+
+@jira_endpoint.route('/search', methods=["POST"])
+@token_required()
+def post_search():
+    try:
+        config = JiraSearchConfig(request.json)
+        if not config.is_valid():
+            return response_json({"error": f"Invalid request body. Expected JiraSearchConfig"}), 400
+
+        (issues, timestamp) = jira_client.paginate(jql=config.get_jql(), access_token=get_access_token(), use_cache=config.is_use_cache(), page_size=config.get_page_size())
+        return response_json({ "timestamp": timestamp, "issues": issues })
+
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+        return response_json({"error": str(e)}), 400
 
 @init_endpoint
 @inject_environment({"TOKEN_FILENAME": lookup_file("storage/token.json")})
